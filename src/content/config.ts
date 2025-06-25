c5202b916c3f5f442990781ab71969c0425dcc1d
Kuba Fila <jakubf@dev-effectivity.pl>
2025-06-25 19:16:48 +0200

    add stuff to content manager

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,7 @@
 // Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
 
-
-
+///////////Portfolio
 const pagePortfolio = (image) => z.object({
   title: z.string(),
   items: z.array( z.object({
@@ -24,7 +23,84 @@ const pagePortfolioCollection = defineCollection({
   schema: pagePortfolioi18n
 });
 
+
+////////////// ABOUT ME
+const pageAbout = (image) => z.object({
+  title: z.string(),
+  description: z.string().optional()
+})
+
+const pageAbouti18n = ({ image }) => z.object({
+  pl: pageAbout(image),
+});
+
+
+const pageAboutCollection = defineCollection({
+  type: "content",
+  schema: pageAbouti18n
+});
+
+////////////// CONTACT
+const pageContact = (image) => z.object({
+  title: z.string(),
+  description: z.string().optional()
+})
+
+const pageContacti18n = ({ image }) => z.object({
+  pl: pageContact(image),
+});
+
+
+const pageContactCollection = defineCollection({
+  type: "content",
+  schema: pageContacti18n
+});
+
+////////////// Oferta
+const pageOffer = (image) => z.object({
+  title: z.string(),
+  description: z.string().optional()
+})
+
+const pageOfferi18n = ({ image }) => z.object({
+  pl: pageOffer(image),
+});
+
+
+const pageOfferCollection = defineCollection({
+  type: "content",
+  schema: pageOfferi18n
+});
+
+
+/// Ustawienia
+const siteSettingsSchema = (image) => z.object({
+  metaTags: z.object({
+    title: z.string(),
+    description: z.string(),
+    keywords: z.string()
+  }),
+  openGraph: z.object({
+    title: z.string(),
+    description: z.string(),
+    image: image()
+  })
+
+});
+
+const siteSettingsSchemai18n = ({ image }) => z.object({
+  pl: siteSettingsSchema(image),
+});
+
+const settingsCollection = defineCollection({
+  type: "content",
+  schema: siteSettingsSchemai18n
+});
 export const collections = {
   // nazwa folderu: nazwa kolekcji
-  'pagePortfolio': pagePortfolioCollection
+  'pagePortfolio': pagePortfolioCollection,
+  'pageAbout': pageAboutCollection,
+  'pageContact': pageContactCollection,
+  'pageOffer': pageOfferCollection,
+  "settings": settingsCollection
 }
---
+++ src/content/settings/settings.md
@@ -0,0 +1,11 @@
+---
+pl:
+  metaTags:
+    title: Hania Lepieszkiewicz - Portfolio a
+    description: Hania Lepieszkiewicz - Portfolio
+    keywords: "Ilustracja, Bajki, "
+  openGraph:
+    title: Hania Lepieszkiewicz
+    description: Portfolio
+    image: /src/assets/Ikonka2.png
+---
