ae6c1564d69bd65f56abf9fff927c5e9a93bf9bc
Kuba Fila <jakubf@dev-effectivity.pl>
2025-06-27 18:08:58 +0200

    collection-test

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,11 @@
 // Import utilities from `astro:content`
-import { z, defineCollection, image } from "astro:content";
-import {  file } from 'astro/loaders'; // Not available with legacy API
+import {z, defineCollection} from "astro:content";
+import {file, glob} from 'astro/loaders'; // Not available with legacy API
 
 ///////////Portfolio
 const pagePortfolio = (image) => z.object({
   title: z.string(),
-  items: z.array( z.object({
+  items: z.array(z.object({
     visible: z.boolean(),
     title: z.string(),
     slug: z.string(),
@@ -15,13 +15,13 @@ const pagePortfolio = (image) => z.object({
   }))
 })
 
-const pagePortfolioi18n = ({ image }) => z.object({
+const pagePortfolioi18n = ({image}) => z.object({
   pl: pagePortfolio(image),
 });
 
 
-const pagePortfolioCollection = defineCollection({
-  loader: file('src/content/pagePortfolio/pagePortfolio.md'),
+const portfolio = defineCollection({
+  loader: glob({pattern: "portfolio.md", base: './src/content/pages'}),
   schema: pagePortfolioi18n
 });
 
@@ -32,13 +32,13 @@ const pageAbout = (image) => z.object({
   description: z.string().optional()
 })
 
-const pageAbouti18n = ({ image }) => z.object({
+const pageAbouti18n = ({image}) => z.object({
   pl: pageAbout(image),
 });
 
 
-const pageAboutCollection = defineCollection({
-  loader: file('src/content/pageAbout/pageAbout.md'),
+const about = defineCollection({
+  loader: glob({pattern: "about.md", base: './src/content/pages'}),
   schema: pageAbouti18n
 });
 
@@ -48,13 +48,13 @@ const pageContact = (image) => z.object({
   description: z.string().optional()
 })
 
-const pageContacti18n = ({ image }) => z.object({
+const pageContacti18n = ({image}) => z.object({
   pl: pageContact(image),
 });
 
 
-const pageContactCollection = defineCollection({
-  loader: file('src/content/pageContact/pageContact.md'),
+const contact = defineCollection({
+  loader: glob({pattern: "contact.md", base: './src/content/pages'}),
   schema: pageContacti18n
 });
 
@@ -64,29 +64,29 @@ const pageOffer = (image) => z.object({
   description: z.string().optional()
 })
 
-const pageOfferi18n = ({ image }) => z.object({
+const pageOfferi18n = ({image}) => z.object({
   pl: pageOffer(image),
 });
 
 
-const pageOfferCollection = defineCollection({
-  loader: file('src/content/pageOffer/pageOffer.md'),
+const offer = defineCollection({
+  loader: glob({pattern: "offer.md", base: './src/content/pages'}),
   schema: pageOfferi18n
 });
 
 ////////////// Główna
-const pageHomepage = (image) => z.object({
+const homepageSchema = (image) => z.object({
   image: image(),
 })
 
-const pageHomepagei18n = ({ image }) => z.object({
-  pl: pageHomepage(image),
+const homepageSchemai18n = ({image}) => z.object({
+  pl: homepageSchema(image),
 });
 
 
-const pageHomepageCollection = defineCollection({
-  loader: file('src/content/pageHomepage/pageHomepage.md'),
-  schema: pageHomepagei18n
+const homepage = defineCollection({
+  loader: glob({pattern: "homepage.md", base: './src/content/pages'}),
+  schema: homepageSchemai18n
 });
 
 ////////////// Error
@@ -95,13 +95,13 @@ const pageError = (image) => z.object({
   description: z.string()
 })
 
-const pageErrori18n = ({ image }) => z.object({
+const pageErrori18n = ({image}) => z.object({
   pl: pageError(image),
 });
 
 
-const pageErrorCollection = defineCollection({
-  loader: file('src/content/pageError/pageError.md'),
+const error = defineCollection({
+  loader: glob({pattern: "error.md", base: './src/content/pages'}),
   schema: pageErrori18n
 });
 
@@ -121,20 +121,20 @@ const metaSettingsSchema = (image) => z.object({
 
 });
 
-const metaSettingsSchemai18n = ({ image }) => z.object({
+const metaSettingsSchemai18n = ({image}) => z.object({
   pl: metaSettingsSchema(image),
 });
 
 const metaSettings = defineCollection({
-  loader: file('src/content/settings/meta.md'),
+  loader: glob({pattern: "meta.md", base: 'src/content/settings'}),
   schema: metaSettingsSchemai18n
 });
 export const collections = {
-  pagePortfolioCollection,
-  pageAboutCollection,
-  pageContactCollection,
-  pageOfferCollection,
-  pageHomepageCollection,
-  pageErrorCollection,
+  portfolio,
+  about,
+  contact,
+  offer,
+  homepage,
+  error,
   metaSettings
 }
